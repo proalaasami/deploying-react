794fc7e17933fdeeac151758b00d54b9364227be
gamal646 <gamal646@gamal646s-MacBook-Air.local>
2020-08-26 07:18:46 +0200

    initial commit

---
+++ 6ix-9ine/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>let's git it done</h1>
+      <p>my react host</p>
+    </div>
+  );
+}
+
+export default App;
