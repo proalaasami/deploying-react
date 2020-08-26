842a2d6e7221264b44a93d0ac1ecc75f4534d6ee
gamal646 <gamal646@gamal646s-MacBook-Air.local>
2020-08-26 07:19:52 +0200

    Updates

---
+++ precache-manifest.e82532c60c92f437778bc2099d0d2008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb149bb86e9ee38101adb4028fd5fb0",
+    "url": "/deploying-react/index.html"
+  },
+  {
+    "revision": "cefcb0d5802934b9f154",
+    "url": "/deploying-react/static/css/main.b33eceb4.chunk.css"
+  },
+  {
+    "revision": "fd369a8722a9143016e3",
+    "url": "/deploying-react/static/js/2.4280edd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploying-react/static/js/2.4280edd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cefcb0d5802934b9f154",
+    "url": "/deploying-react/static/js/main.c0e781ac.chunk.js"
+  },
+  {
+    "revision": "e7aea21fb54d41af050a",
+    "url": "/deploying-react/static/js/runtime-main.8c79ea53.js"
+  }
+]);
